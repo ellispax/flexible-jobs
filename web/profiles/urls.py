87816553b106f096ffef9720f7dc01a8d318bcfc
Ellis Pax <mapakameellis@gmail.com>
2023-07-18 21:06:52 +0700

    added education profile and work experience profiles for users.

--- web/profiles/urls.py
+++ web/profiles/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     
     path('general-profile/<int:pk>',get_general_user_profile, name="general-profile"),
-    path('company-profile/<int:pk>', get_company_user_profile, name='create-company-user' )
+    path('company-profile/<int:pk>', get_company_user_profile, name='create-company-user' ),
+    path('education-profile/<int:pk>', user_education_profile, name='education-profile'),
+    path('add-skill', skills, name='add-skill'),
 ]
