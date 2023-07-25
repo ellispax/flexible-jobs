9e4b7b7affc66c4113197ae58d7421dc67d36811
Ellis Pax <mapakameellis@gmail.com>
2023-07-25 19:21:57 +0700

    company app edits

--- web/company/views.py
+++ web/company/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-
 from rest_framework import status, serializers
 from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.authentication import TokenAuthentication
@@ -8,28 +6,48 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import CompanySerializer
 from .models import Company
+from profiles.models import CompanyUserProfile
 from main.permissions import IsGeneralUser, IsCompanyUser, IsAdminUser
 
 
 # Create your views here.
 
-@api_view(['POST'])         #creating a new company
+@api_view(['GET','POST','DELETE'])         #creating a new company
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated, IsAdminUser])
 def create_company(request):
-    serializer = CompanySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT','DELETE'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated, IsAdminUser])           #get company profile using pk
-def company(request, pk):
+    user = request.user
     try:
         company = Company.objects.get(pk=pk)
+    except Company.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CompanySerializer(data=request.data)
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        company.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+    # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated, IsCompanyUser])           #get company profile using pk
+def company(request):
+    try:
+        user = request.user # get authenticated user        
+        
+        company_profile = CompanyUserProfile.objects.get(user=user)     # Retrieve the CompanyUserProfile related to the user
+        
+        company_id = company_profile.company_id      # Use the company_id to fetch the company's profile        
+        company = Company.objects.get(pk=company_id     )# Replace the pk value with company_id
     except Company.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -44,7 +62,5 @@ def company(request, pk):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-    elif request.method == 'DELETE':
-        company.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    
     
