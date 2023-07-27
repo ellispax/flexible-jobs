a306df7a13bcc51f0e5dda6f46efc5415ec2cfb7
Ellis Pax <mapakameellis@gmail.com>
2023-07-27 11:56:14 +0700

    update user profiles

--- web/users/urls.py
+++ web/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path('login', login, name="login" ),
     path('register',register, name="register"),
     path('register-company', create_company_user, name='create-company-user' ),
-    path('login/', custom_login, name='login'),
+    path('login', custom_login, name='login'),
 ]
