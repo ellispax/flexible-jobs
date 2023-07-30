82dbf58192ae82d4cc6de35fc826e0ab3f45969f
Ellis Pax <mapakameellis@gmail.com>
2023-07-30 23:28:00 +0700

    register user view added

--- web/profiles/urls.py
+++ web/profiles/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('user-profile', get_user_profile, name='user-profile'),
     path('general-profile',get_general_user_profile, name="general-profile"),
     path('company-profile', get_company_user_profile, name='create-company-user' ),
+    path('work-profile', user_work_profile,name='work-profile'),
     path('education-profile', user_education_profile, name='education-profile'),
     path('add-skill', skills, name='add-skill'),
     
